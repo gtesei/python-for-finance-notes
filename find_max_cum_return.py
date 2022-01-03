a1a582f9f2511e4bae904ef0df1db52890da626f
Gino Tesei <gtesei@yahoo.com>
2022-01-02 22:44:08 -0800

    Update find_max_cum_return.py

--- find_max_cum_return.py
+++ find_max_cum_return.py
@@ -8,6 +8,58 @@ from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 import argparse
 import datetime
 import random
+import logging
+
+def get_logger(log_dir, name):
+    """Get a `logging.Logger` instance that prints to the console
+    and an auxiliary file.
+    Args:
+        log_dir (str): Directory in which to create the log file.
+        name (str): Name to identify the logs.
+    Returns:
+        logger (logging.Logger): Logger instance for logging events.
+    """
+    class StreamHandlerWithTQDM(logging.Handler):
+        """Let `logging` print without breaking `tqdm` progress bars.
+        See Also:
+            > https://stackoverflow.com/questions/38543506
+        """
+        def emit(self, record):
+            try:
+                msg = self.format(record)
+                tqdm.write(msg)
+                self.flush()
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                self.handleError(record)
+
+    # Create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # Log everything (i.e., DEBUG level and above) to a file
+    log_path = os.path.join(log_dir, f'{name}.txt')
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Log everything except DEBUG level (i.e., INFO level and above) to console
+    console_handler = StreamHandlerWithTQDM()
+    console_handler.setLevel(logging.INFO)
+
+    # Create format for the logs
+    file_formatter = logging.Formatter('[%(asctime)s] %(message)s',
+                                       datefmt='%m.%d.%y %H:%M:%S')
+    file_handler.setFormatter(file_formatter)
+    console_formatter = logging.Formatter('[%(asctime)s] %(message)s',
+                                          datefmt='%m.%d.%y %H:%M:%S')
+    console_handler.setFormatter(console_formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
 
 
 def get_data(symbols, 
@@ -67,6 +119,7 @@ if __name__ == "__main__":
     print('-' * 30)
     
     ##
+    log = get_logger('.', 'find_max_cum_ret')
     nasdaq_symbols = get_nasdaq_symbols()
     stock_list = list(nasdaq_symbols.index)
     random.shuffle(stock_list)
